3a8b18d6f8634144503683c83ca57af1cc401e91
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2016-12-16 13:46:36 +0000

    Broadcasts door events to collector

--- lib/ir_beam_worker.ex
+++ lib/ir_beam_worker.ex
@@ -10,11 +10,37 @@ defmodule IrBeamWorker do
   def init([]) do
     {:ok, pid} = Gpio.start_link(@ir_in, :input)
     Gpio.set_int(pid, :both)
-    {:ok, pid}
+    evt = case Gpio.read(pid) do
+      1 ->
+        build_event(:falling)
+      0 ->
+        build_event(:rising)
+    end
+    broadcast(evt)
+    {:ok, :ignored}
   end
 
-  def handle_info(evt, state) do
-    IO.inspect evt
+  def handle_info({:gpio_interrupt, @ir_in, edge}, state) do
+    build_event(edge)
+    |> broadcast
     {:noreply, state}
+  end
+
+  defp build_event(edge) do
+    %{office: "london",
+      timestamp: :calendar.universal_time(),
+      action: action(edge)}
+  end
+
+  defp action(:falling), do: :open
+  defp action(:rising), do: :close
+
+  defp broadcast(evt) do
+    try do
+      IO.inspect evt
+      GenEvent.notify(:collector, evt)
+    rescue
+      e -> IO.puts "Notify failed: #{inspect e}"
+    end
   end
 end
